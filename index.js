890bbb0979e821db83c54da1fb1b5dc53a8366b4
vanessa garcia <venessag0908@gmail.com>
2023-03-21 17:29:48 -0600

    arithmeticlab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2
+let num3 = 9;
+let num4 = 5;
+const mod = num3 % num4
+const max = Math.max(5, 10, 15, 20);
+const random = Math.floor(Math.random() * 10) + 1;
